605e643e61901b2f61cfd03993dcbd58f3a6562a
Eki Bagas <ekibagas99@gmail.com>
2025-04-02 14:24:55 +0700

    update banner

--- src/components/Loading/HomeLoading.js
+++ src/components/Loading/HomeLoading.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Grid, Grow, Container, Box } from '@mui/material'
-import { styled } from '@mui/material/styles'
+import React from "react";
+import { Grid, Grow, Container, Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const MyBox = styled(Box)(({ theme }) => ({
   width: 806,
-  height: '106px',
-  [theme.breakpoints.down('sm')]: {
+  height: "106px",
+  [theme.breakpoints.down("sm")]: {
     width: 355,
     height: 80,
   },
@@ -13,79 +13,79 @@ const MyBox = styled(Box)(({ theme }) => ({
   //   width: 50,
   //   height: 20,
   // },
-}))
+}));
 
 const HomeLoading = () => {
-  const categories = [0, 1, 2, 3, 4, 5, 6]
-  const posts = [0, 1, 2, 3, 4]
+  const categories = [0, 1, 2, 3, 4, 5, 6];
+  const posts = [0, 1, 2, 3, 4];
   return (
     <Grow in>
-      <Container maxWidth='xl'>
+      <Container maxWidth="xl">
         <Grid
           container
-          aligntitems='center'
+          aligntitems="center"
           spacing={0}
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
           }}
         >
-          <Grid item xs={12} sx={{ marginTop: '20px', display: 'flex' }}>
+          <Grid item xs={12} sx={{ marginTop: "20px", display: "flex" }}>
             <Box
               sx={{
-                margin: 'auto',
-                width: '1500px',
-                height: '300px',
-                borderRadius: '8px',
-                backgroundColor: 'rgba(236, 236, 236, 0.7)',
+                margin: "auto",
+                width: "1500px",
+                height: "300px",
+                borderRadius: "8px",
+                backgroundColor: "rgba(236, 236, 236, 0.7)",
               }}
             />
           </Grid>
 
-          <Grid item sx={{ marginTop: '20px' }}>
+          <Grid item sx={{ marginTop: "20px" }}>
             <Box
               sx={{
-                margin: 'auto',
-                width: '100px',
-                height: '10px',
-                borderRadius: '8px',
-                backgroundColor: 'rgba(236, 236, 236, 0.7)',
+                margin: "auto",
+                width: "100px",
+                height: "10px",
+                borderRadius: "8px",
+                backgroundColor: "rgba(236, 236, 236, 0.7)",
               }}
             />
           </Grid>
           <Grid
             item
             xs={12}
-            sx={{ display: 'flex', margin: '20px auto 0px auto' }}
+            sx={{ display: "flex", margin: "20px auto 0px auto" }}
           >
             {categories.map((category) => (
               <Box
                 key={category}
                 sx={{
-                  margin: 'auto 9px',
-                  width: '100px',
-                  height: '30px',
-                  borderRadius: '8px',
-                  backgroundColor: 'rgba(236, 236, 236, 0.7)',
+                  margin: "auto 9px",
+                  width: "100px",
+                  height: "30px",
+                  borderRadius: "8px",
+                  backgroundColor: "rgba(236, 236, 236, 0.7)",
                 }}
               />
             ))}
           </Grid>
 
-          <Grid item xs={12} sx={{ marginTop: '20px', margin: 'auto' }}>
+          <Grid item xs={12} sx={{ marginTop: "20px", margin: "auto" }}>
             {posts.map((post) => (
               <Grid
                 key={post}
                 item
                 xs={12}
                 sx={{ margin: 1 }}
-                alignItems='center'
+                alignItems="center"
               >
                 <MyBox
                   sx={{
-                    margin: 'auto',
-                    backgroundColor: 'rgba(236, 236, 236, 0.7)',
+                    margin: "auto",
+                    backgroundColor: "rgba(236, 236, 236, 0.7)",
                   }}
                 />
               </Grid>
@@ -94,7 +94,7 @@ const HomeLoading = () => {
         </Grid>
       </Container>
     </Grow>
-  )
-}
+  );
+};
 
-export default HomeLoading
+export default HomeLoading;
